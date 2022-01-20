7b3dae48ad550882299750b8c4653f99f0c19360
aimeliala <ahmadhakim404@gmail.com>
2022-01-20 16:47:36 +0700

    make our first component with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import logo from './logo.svg';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello, React</h1>
-    </div>
+    <Router>
+      <div className="container">
+        <div className="row">
+          <h1 className="mt-3">
+            Go! Watch some movies.
+          </h1>
+          <hr className="mb-3" />
+        </div>
+        <div className="row">
+          <div className="col-md-2">
+            <nav>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/movies">Movies</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/admin">Admin Panel</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="col-md-10">
+            <Routes>
+              <Route path="/movies" element={<Movies/>} />
+              <Route path="/admin" element={<Admin/>} />
+              <Route path="/" element={<Home/>} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
+}
+
+function Home(){
+  return <h1>Home</h1>
+}
+function Movies(){
+  return <h1>Movies</h1>
+}
+function Admin(){
+  return <h1>Admin</h1>
 }
 
 export default App;
