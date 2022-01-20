73f60334cf9e71accb97de8d03ae9a66be52a4cd
aimeliala <ahmadhakim404@gmail.com>
2022-01-20 17:16:51 +0700

    example show movies data with usestate and useeffect

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Movies = () => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        setMovies([
+            {id: 1, title: 'Shark Tale'},
+            {id: 2, title: 'Sing 2'},
+            {id: 3, title: 'Frozen'},
+        ])
+    })
+
     return (
         <>
-            <h1>Movies</h1>
+            <ul>
+                {movies.map((movie) => (
+                    <li key={movie.id}>
+                        {movie.title}
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
