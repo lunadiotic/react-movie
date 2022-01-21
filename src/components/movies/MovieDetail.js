d622c528bf73405f04036bf9b63395223087c6b6
aimeliala <ahmadhakim404@gmail.com>
2022-01-22 01:34:02 +0700

    layout - make movie detail page

--- src/components/movies/MovieDetail.js
+++ src/components/movies/MovieDetail.js
@@ -15,7 +15,21 @@ const MovieDetail = () => {
     
     return (
         <>
-            <h2>Movie: {movie.title}</h2>
+            <h2>{movie.title}</h2>
+            <div className="card shadow-sm">
+                <svg
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height={400}
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    preserveAspectRatio="xMidYMid slice"
+                    focusable="false"
+                >
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#55595c" />
+                </svg>
+            </div>
             <table className="table table-dark table-striped">
                 <tbody>
                     <tr>
